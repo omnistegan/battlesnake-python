7bf6aa4d35dfb5ac83b4362d855626de33762aa2
dinosawrusrex <dennissaw12@gmail.com>
2018-03-01 00:47:44 -0800

    Made safe space implicit to Cell class.

--- app/main.py
+++ app/main.py
@@ -36,24 +36,28 @@ class Cell:
         self.is_snakenemy = False # head of enemy snake(s)
         self.is_snakebody = False
         self.is_snaketail = False
+        self.safe = True
         self.snake_id = None
         self.is_food = False
         self.coord = (row, column)
         self.symbol = {'snakehead': 's', 'snakenemy': 'e', 
                        'snakebody': 'b', 'snaketail': 't', 
-                       'food': 'f', 'cell': '_'}
+                       'food': 'f', 'cell': '_',
+                       'danger': '!'}
     
     def to_symbol(self):
         if self.is_snakehead == True:
             return(self.symbol['snakehead'])
         elif self.is_snakenemy == True:
             return(self.symbol['snakenemy'])
-        elif self.is_food == True:
-            return(self.symbol['food'])
         elif self.is_snakebody == True:
             return(self.symbol['snakebody'])
         elif self.is_snaketail == True:
             return(self.symbol['snaketail'])
+        elif self.safe == False:
+            return(self.symbol['danger'])
+        elif self.is_food == True:
+            return(self.symbol['food'])
         else:
             return(self.symbol['cell'])
 
@@ -70,27 +74,45 @@ class Grid:
                 print(cell.to_symbol(), end=" ")
             print("")
 
-    def place(self, instance, obj): # maybe break into 3 separate functions?
-        if obj == 'food':
-            for i in range(len(instance)):
-                self.coord[instance[i].coord[0]][instance[i].coord[1]].is_food = True
-        elif obj == 'enemy':
-            for i in range(len(instance)):
-                self.coord[instance[i].head[0]][instance[i].head[1]].is_snakenemy = True
-                self.coord[instance[i].head[0]][instance[i].head[1]].snake_id = instance[i].id
-                self.coord[instance[i].tail[0]][instance[i].tail[1]].is_snaketail = True
-                self.coord[instance[i].tail[0]][instance[i].tail[1]].snake_id = instance[i].id
-                for j in range(len(instance[i].body)):
-                    self.coord[instance[i].body[j][0]][instance[i].body[j][1]].is_snakebody = True
-                    self.coord[instance[i].body[j][0]][instance[i].body[j][1]].snake_id = instance[i].id
-        elif obj == 'me':
-            self.coord[instance.head[0]][instance.head[1]].is_snakehead = True
-            self.coord[instance.head[0]][instance.head[1]].snake_id = instance.id
-            self.coord[instance.tail[0]][instance.tail[1]].is_snaketail = True    
-            self.coord[instance.tail[0]][instance.tail[1]].snake_id = instance.id
-            for j in range(len(instance.body)):
-                self.coord[instance.body[j][0]][instance.body[j][1]].is_snakebody = True
-                self.coord[instance.body[j][0]][instance.body[j][1]].snake_id = instance.id
+    def food_place(self, nourriture): 
+        for i in range(len(nourriture)):
+            self.coord[nourriture[i].coord[0]][nourriture[i].coord[1]].is_food = True
+
+    def enemy_place(self, enemy):
+        self.coord[enemy.head[0]][enemy.head[1]].is_snakenemy = True
+        self.coord[enemy.head[0]][enemy.head[1]].safe = False
+        self.coord[enemy.head[0]][enemy.head[1]].snake_id = enemy.id
+
+        self.coord[enemy.tail[0]][enemy.tail[1]].is_snaketail = True
+        self.coord[enemy.tail[0]][enemy.tail[1]].snake_id = enemy.id
+
+        for j in range(len(enemy.body)):
+            self.coord[enemy.body[j][0]][enemy.body[j][1]].is_snakebody = True
+            self.coord[enemy.body[j][0]][enemy.body[j][1]].safe = False
+            self.coord[enemy.body[j][0]][enemy.body[j][1]].snake_id = enemy.id
+            
+        if enemy.longer_than_me == True:
+            for i in range(-1,2):
+                self.coord[enemy.head[0]+i][enemy.head[1]].safe = False
+                self.coord[enemy.head[0]][enemy.head[1]+i].safe = False
+
+        if enemy.dist_closestfood == 1:
+            self.coord[enemy.tail[0]][enemy.tail[1]].safe = False
+
+    def me_place(self, moi):
+            self.coord[moi.head[0]][moi.head[1]].is_snakehead = True
+            self.coord[moi.head[0]][moi.head[1]].snake_id = moi.id
+
+            self.coord[moi.tail[0]][moi.tail[1]].is_snaketail = True    
+            self.coord[moi.tail[0]][moi.tail[1]].snake_id = moi.id
+
+            for j in range(len(moi.body)):
+                self.coord[moi.body[j][0]][moi.body[j][1]].is_snakebody = True
+                self.coord[moi.body[j][0]][moi.body[j][1]].safe = False
+                self.coord[moi.body[j][0]][moi.body[j][1]].snake_id = moi.id
+
+            if moi.dist_closestfood == 1:
+                self.coord[moi.tail[0]][moi.tail[1]].safe = False
                  
 
 class Food:
@@ -98,14 +120,14 @@ class Food:
         self.coord = [prepend['y'], prepend['x']]
 
     def order(nourriture, cls):
-        foods = [(nourriture[i], distance(cls, nourriture[i].coord))
+        fods = [(nourriture[i], distance(cls, nourriture[i].coord))
                  for i in range(len(nourriture))]
-        foods_ordered = sorted(foods, key = lambda foods: foods[1])
+        foods_ordered = sorted(fods, key = lambda fods: fods[1])
         foods_reordered = [item[0] for item in foods_ordered]
         return(foods_reordered)
 
 class Enemy:
-    def __init__(self, prepend, nourriture):
+    def __init__(self, prepend, moi, nourriture):
         self.head = [prepend['body']['data'][0]['y'],
                      prepend['body']['data'][0]['x']]
         self.tail = [prepend['body']['data'][-1]['y'], 
@@ -114,6 +136,7 @@ class Enemy:
                       prepend['body']['data'][j]['x']] 
                       for j in range(1, len(prepend['body']['data'])-1)]
         self.length = prepend['length']
+        self.longer_than_me = self.length >= moi.length
         self.id = prepend['id']
         self.foods_ordered = Food.order(nourriture, self)
         self.dist_closestfood = distance(self, self.foods_ordered[0].coord)
@@ -161,24 +184,8 @@ def safe(agrid, moi, enemy, prepend):
         if (0 <= directions[key][0] < prepend['height'] 
                 and 0 <= directions[key][1] < prepend['width'] 
                 and agrid.coord[directions[key][0]][directions[key][1]].\
-                    is_snakebody == False 
-                and agrid.coord[directions[key][0]][directions[key][1]].\
-                    is_snakenemy == False): 
-            if agrid.coord[directions[key][0]][directions[key][1]].\
-                        is_snaketail == True:
-                if agrid.coord[directions[key][0]][directions[key][1]].\
-                            snake_id != moi.id:
-                    target_snake = [target for target in enemy 
-                                    if target.id == agrid.coord\
-                                            [directions[key][0]][directions[key][1]].snake_id]
-                    if distance(target_snake[0], target_snake[0].foods_ordered[0].coord) > 1:
-                        space.append(key)
-                elif agrid.coord[directions[key][0]][directions[key][1]].snake_id == moi.id: 
-                    # Only valid when goal is food
-                    if distance(moi, moi.foods_ordered[0].coord) > 1:
-                        space.append(key)
-            else:
-                space.append(key)
+                    safe == True): 
+            space.append(key)
 
     return(space)
     
@@ -214,14 +221,14 @@ def move():
 
     me = Me(data['you'], foods) 
 
-    enemies = [Enemy(data['snakes']['data'][i], foods) 
+    enemies = [Enemy(data['snakes']['data'][i], me, foods) 
                for i in range(len(data['snakes']['data'])) 
                if data['snakes']['data'][i]['id'] != me.id]
 
     # Grid for log purposes
-    grid.place(foods, 'food')
-    grid.place(enemies, 'enemy')
-    grid.place(me, 'me')
+    grid.food_place(foods)
+    for enemoir in enemies: grid.enemy_place(enemoir)
+    grid.me_place(me)
     grid.print()
     
     # Route setter
@@ -240,6 +247,7 @@ def move():
     print('route: %s' % (route))
     print('safety: %s' % (safety))
     print('output: %s' % (output))
+    print(enemies[0].longer_than_me)
 
     return {
         'move': output,
