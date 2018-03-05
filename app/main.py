f83fc15330bef2b6cbddcab35dde32eda2a446a3
Graham Bouvier <graham@freerunningtechnologies.com>
2018-03-05 13:17:02 -0800

    WIP

--- app/main.py
+++ app/main.py
@@ -28,110 +28,102 @@ def start():
 
 ################################################################################
 
+PRIORIZE_FOOD_THRESHOLD = 30
+SYMBOL_MAP = {
+    'snakehead': 's',
+    'snakenemy': 'e',
+    'snakebody': 'b',
+    'snaketail': 't',
+    'food': 'f',
+    'empty': '_'
+}
 
 class Cell:
     def __init__(self, row, column):
-        self.is_snakehead = False
-        self.is_snakenemy = False # head of enemy snake(s)
-        self.is_snakebody = False
-        self.is_snaketail = False
+        self.state = 'empty'
         self.safe = True
         self.snake_id = None
-        self.is_food = False
         self.coord = (row, column)
-        self.symbol = {'snakehead': 's', 'snakenemy': 'e',
-                       'snakebody': 'b', 'snaketail': 't',
-                       'food': 'f', 'cell': '_',
-                       'danger': '!'}
 
     def to_symbol(self):
-        if self.is_snakehead == True:
-            return(self.symbol['snakehead'])
-        elif self.is_snakenemy == True:
-            return(self.symbol['snakenemy'])
-        elif self.is_snakebody == True:
-            return(self.symbol['snakebody'])
-        elif self.is_snaketail == True:
-            return(self.symbol['snaketail'])
-        elif self.safe == False:
-            return(self.symbol['danger'])
-        elif self.is_food == True:
-            return(self.symbol['food'])
-        else:
-            return(self.symbol['cell'])
+        return(SYMBOL_MAP[self.state])
 
 
 class Grid:
     def __init__(self, prepend):
         self.height = prepend['height']
         self.width = prepend['width']
-        self.coord = [[Cell(row, col)
+        self.grid = [[Cell(row, col)
                        for col in range(self.width)]
                        for row in range(self.height)]
 
-    def print(self):
-        for row in self.coord:
+    def neighbors(self, coord):
+        y, x = coord
+        neighbors = []
+        for i in range(-1, 2, 2):
+            if 0 <= x+i < self.height:
+                neighbors.append((y, x+i))
+            if 0 <= y+i < self.width:
+                neighbors.append((y+i, x))
+        return neighbors
+
+    def safe(self, coord):
+        if coord[0] < 0 or coord[1] < 0 or coord[0] >= self.height or coord[1] >= self.width:
+            return False
+        else:
+            return(self.cell_at(coord).safe)
+
+    def safeish(self, coord):
+        if coord[0] < 0 or coord[1] < 0 or coord[0] >= self.height or coord[1] >= self.width:
+            return False
+        elif cell_at(coord).state in ['empty', 'snaketail', 'food']:
+            return True
+        else:
+            return False
+
+    def cell_at(self, coord):
+        y, x = coord
+        return(self.grid[x][y])
+
+    def set_at(self, coord, state, snake_id=None, safe=True):
+        y, x = coord
+        self.grid[x][y].state = state
+        self.grid[x][y].snake_id = snake_id
+        self.grid[x][y].safe = safe
+
+    def print_grid(self):
+        for row in self.grid:
             for cell in row:
                 print(cell.to_symbol(), end=" ")
             print("")
 
-    def food_place(self, nourriture):
-        for i in range(len(nourriture)):
-            self.coord[nourriture[i].coord[0]][nourriture[i].coord[1]].is_food = True
+    def place_food(self, food_list):
+        for food in food_list:
+            self.set_at(food.coord, 'food')
 
-    def enemy_place(self, enemy):
-        self.coord[enemy.head[0]][enemy.head[1]].is_snakenemy = True
-        self.coord[enemy.head[0]][enemy.head[1]].safe = False
-        self.coord[enemy.head[0]][enemy.head[1]].snake_id = enemy.id
+    def place_enemy(self, enemy):
+        self.set_at(enemy.head, 'snakenemy', enemy.id, False)
+        for body in enemy.body:
+            self.set_at(body, 'snakebody', enemy.id, False)
+        self.set_at(enemy.tail, 'snaketail', enemy.id, not enemy.dist_closest_food == 1)
 
-        self.coord[enemy.tail[0]][enemy.tail[1]].is_snaketail = True
-        self.coord[enemy.tail[0]][enemy.tail[1]].snake_id = enemy.id
+        if enemy.longer_than_me:
+            for neighbor in self.neighbors(enemy.head):
+                self.set_at(neighbor, 'empty', None, False)
 
-        for j in range(len(enemy.body)):
-            self.coord[enemy.body[j][0]][enemy.body[j][1]].is_snakebody = True
-            self.coord[enemy.body[j][0]][enemy.body[j][1]].safe = False
-            self.coord[enemy.body[j][0]][enemy.body[j][1]].snake_id = enemy.id
-
-        if enemy.longer_than_me == True:
-            for i in range(-1,2):
-                if 0 <= enemy.head[0]+i < self.height:
-                    self.coord[enemy.head[0]+i][enemy.head[1]].safe = False
-                if 0 <= enemy.head[1]+i < self.width:
-                    self.coord[enemy.head[0]][enemy.head[1]+i].safe = False
-
-        if enemy.dist_closest_food == 1:
-            self.coord[enemy.tail[0]][enemy.tail[1]].safe = False
-
-    def me_place(self, moi):
-        self.coord[moi.head[0]][moi.head[1]].is_snakehead = True
-        #self.coord[moi.head[0]][moi.head[1]].safe = False
-        self.coord[moi.head[0]][moi.head[1]].snake_id = moi.id
-
-        self.coord[moi.tail[0]][moi.tail[1]].is_snaketail = True
-        self.coord[moi.tail[0]][moi.tail[1]].snake_id = moi.id
-
-        for j in range(len(moi.body)):
-            self.coord[moi.body[j][0]][moi.body[j][1]].is_snakebody = True
-            self.coord[moi.body[j][0]][moi.body[j][1]].safe = False
-            self.coord[moi.body[j][0]][moi.body[j][1]].snake_id = moi.id
-
-        if moi.dist_closest_food == 1:
-            self.coord[moi.tail[0]][moi.tail[1]].safe = False
+    def place_me(self, me):
+        self.set_at(me.head, 'snakehead', me.id, False)
+        for body in me.body:
+            self.set_at(body, 'snakebody', me.id, False)
+        self.set_at(me.tail, 'snaketail', me.id, not me.dist_closest_food == 1)
 
 
 class Food:
     def __init__(self, prepend):
         self.coord = [prepend['y'], prepend['x']]
 
-    def order(nourriture, cls):
-        fods = [(nourriture[i], distance(cls, nourriture[i].coord))
-                 for i in range(len(nourriture))]
-        foods_ordered = sorted(fods, key = lambda fods: fods[1])
-        foods_reordered = [item[0] for item in foods_ordered]
-        return(foods_reordered)
-
 class Snake:
-    def __init__(self, prepend, nourriture):
+    def __init__(self, prepend, food_list):
         self.head = [prepend['body']['data'][0]['y'],
                      prepend['body']['data'][0]['x']]
         self.tail = [prepend['body']['data'][-1]['y'],
@@ -141,145 +133,146 @@ class Snake:
                       for j in range(1, len(prepend['body']['data'])-1)]
         self.length = prepend['length']
         self.id = prepend['id']
-        self.foods_ordered = Food.order(nourriture, self)
-        self.dist_closest_food = distance(self, self.foods_ordered[0].coord)
+        self.foods_ordered = ordered_objects(food_list, self.head)
+        self.dist_closest_food = distance(self.head, self.foods_ordered[0].coord)
 
 class Enemy(Snake):
-    def __init__(self, prepend, moi, nourriture):
-        super().__init__(prepend, nourriture)
-        self.longer_than_me = self.length >= moi.length
-
-        # distance to food
-        # distance to me
+    def __init__(self, prepend, me, food_list):
+        super().__init__(prepend, food_list)
+        self.longer_than_me = self.length >= me.length
 
 class Me(Snake):
-    def __init__(self, prepend, nourriture):
-        super().__init__(prepend, nourriture)
+    def __init__(self, prepend, food_list):
+        super().__init__(prepend, food_list)
         self.health = prepend['health']
+
+
+class Game:
+    def __init__(self, prepend):
+        self.grid = Grid(prepend)
+        self.foods = [Food(food_prepend)
+                      for food_prepend in prepend['food']['data']]
+        self.me = Me(prepend['you'], self.foods)
+        self.enemies = [Enemy(enemy_prepend, self.me, self.foods)
+                        for enemy_prepend in prepend['snakes']['data']
+                        if enemy_prepend['id'] != self.me.id]
+
+        self.grid.place_food(self.foods)
+        for enemy in self.enemies:
+            self.grid.place_enemy(enemy)
+        self.grid.place_me(self.me)
+        self.grid.print_grid()
+
+    def goal_set(self):
+        if self.me.health <= PRIORIZE_FOOD_THRESHOLD:
+            output = self.me.foods_ordered[0].coord
+            log = "food"
+        else:
+            output = self.target_tail()
+            log = "tail"
+        return(output, log)
+
+    def target_tail(self):
+        output = []
+        target_snake = [sorted(self.enemies, key = lambda enemy: distance(self.me.head, enemy.tail))[0]]
+        tail_coord = target_snake.tail
+        if self.grid.safe(tail_coord):
+            output.append(tail_coord)
+
+        target_dir = find_target_direction(target_snake)
+
+        for i in range(1, 3):
+            if target_dir == 'down':
+                    coord = (target_coord[0]+i, target_coord[1])
+                    if self.grid.safe(coord):
+                        output.append(coord)
+            elif target_dir == 'up':
+                    coord = (target_coord[0]-i, target_coord[1])
+                    if self.grid.safe(coord):
+                        output.append(coord)
+            elif target_dir == 'right':
+                    coord = (target_coord[0], target_coord[1]+i)
+                    if self.grid.safe(coord):
+                        output.append(coord)
+            elif target_dir == 'left':
+                    coord = (target_coord[0], target_coord[1]-i)
+                    if self.grid.safe(coord):
+                        output.append(coord)
+
+        return(output)
+
+    def safe_coords(self):
+        safe = []
+        for coord in self.grid.neighbors(self.me.head):
+            if self.grid.safe(coord):
+                safe.append(coord)
+
+        if len(safe) == 0:
+            for coord in self.grid.neighbors(self.me.head):
+                if self.grid.safeish(coord):
+                    safe.append(coord)
+
+        return(safe)
+
+    def safe_directions(self):
+        safe_directions = []
+        safe_coords = self.safe_coords()
+        for choice in safe_coords:
+            if choice[0] > self.me.head[0]:
+                safe_directions.append('down')
+            elif choice[0] < self.me.head[0]:
+                safe_directions.append('up')
+            elif choice[1] > self.me.head[1]:
+                safe_directions.append('right')
+            elif choice[1] < self.me.head[1]:
+                safe_directions.append('left')
+        return safe_directions
 
 
 ################################################################################
 
 
+def find_target_direction(target_snake):
+    tail = target_snake.tail
+    if target_snake.tail == target_snake.body[-1]:
+        body = target_snake.body[-2]
+    else:
+        body = target_snake.body[-1]
+
+    if body[0] < tail[0]:
+        return 'bottom'
+    elif body[0] > tail[0]:
+        return 'top'
+    elif body[1] < tail[1]:
+        return 'right'
+    elif body[1] > tail[1]:
+        return 'left'
+
+
+def ordered_objects(obj_list, target):
+    return(sorted(obj_list, key = lambda obj: distance(obj.coord, target)))
+
+
 def distance(frm, to):
-    dy = abs(to[0] - frm.head[0])
-    dx = abs(to[1] - frm.head[1])
+    dy = abs(to[0] - frm[0])
+    dx = abs(to[1] - frm[1])
     return(sum([dy, dx]))
 
 
-def path(frm, to, agrid):
-    possible = []
+def path(frm, to):
+    possibilities = []
 
-    if to[0]>frm.head[0]:
-        possible.append('down')
-    elif to[0]<frm.head[0]:
-        possible.append('up')
+    if to[0]>frm[0]:
+        possibilities.append('down')
+    elif to[0]<frm[0]:
+        possibilities.append('up')
 
-    if to[1]>frm.head[1]:
-        possible.append('right')
-    elif to[1]<frm.head[1]:
-        possible.append('left')
+    if to[1]>frm[1]:
+        possibilities.append('right')
+    elif to[1]<frm[1]:
+        possibilities.append('left')
 
-    return(possible)
-
-
-def goal_set(moi, enemoir, agrid):
-    if moi.health <= 30:
-        output = moi.foods_ordered[0].coord
-        log = "food"
-    else:
-        output = target_tail(enemoir, moi, agrid)
-        log = "tail"
-    return(output, log)
-
-
-def safe(agrid, moi, enemy, prepend):
-    directions = {
-            'up': [moi.head[0]-1, moi.head[1]],
-            'down': [moi.head[0]+1, moi.head[1]],
-            'left': [moi.head[0], moi.head[1]-1],
-            'right': [moi.head[0], moi.head[1]+1],
-            }
-
-    space = []
-    backup_space = []
-
-    for key in directions:
-        if (0 <= directions[key][0] < agrid.height
-                and 0 <= directions[key][1] < agrid.width
-                and agrid.coord[directions[key][0]][directions[key][1]].\
-                    safe == True):
-            space.append(key)
-
-    for key in directions:
-        if (0 <= directions[key][0] < agrid.height
-                and 0 <= directions[key][1] < agrid.width
-                and agrid.coord[directions[key][0]][directions[key][1]].\
-                    is_snakenemy == False
-                and agrid.coord[directions[key][0]][directions[key][1]].\
-                    is_snakebody == False):
-            if agrid.coord[directions[key][0]][directions[key][1]].\
-                        is_snaketail == True:
-                if agrid.coord[directions[key][0]][directions[key][1]].\
-                        snake_id != moi.id:
-                    target_snake = [target for target in enemy
-                                    if target.id == agrid.coord\
-                                            [directions[key][0]]\
-                                            [directions[key][1]].snake_id]
-                    if target_snake[0].dist_closest_food > 1:
-                        backup_space.append(key)
-            else:
-                backup_space.append(key)
-
-    return(space, backup_space)
-
-def target_tail(enemoir, moi, agrid):
-        enemy_unordered = [(enemoir[i], distance(moi, enemoir[i].tail))
-                        for i in range(len(enemoir))]
-        enemy_ordered = sorted(enemy_unordered, key = lambda enemy_unordered:\
-                enemy_unordered[1])
-        enemy = [item[0] for item in enemy_unordered]
-        target = enemy[0]
-
-        output = [target.tail]
-
-        if target.length > 3:
-            if target.tail == target.body[-1]: # Checks if tail is same as last body segment, then look at second last body segment instead
-                segment = -2
-            else:
-                segment = -1
-
-            if target.tail[0] == target.body[segment][0]:
-                if target.tail[1] < target.body[segment][1]:
-                    for i in range(-1, -3, -1):
-                        if (target.tail[1]+i >= 0
-                            and agrid.coord[target.tail[0]][target.tail[1]+i].\
-                                    safe == True):
-                            output.append([target.tail[0], target.tail[1]+i])
-
-                elif target.tail[1] > target.body[segment][1]:
-                    for i in range(-1, -3, -1):
-                        if (target.tail[1]-i < agrid.width
-                            and agrid.coord[target.tail[0]][target.tail[1]-i].\
-                                    safe == True):
-                            output.append([target.tail[0], target.tail[1]-i])
-
-            elif target.tail[1] == target.body[segment][1]:
-                if target.tail[0] < target.body[segment][0]:
-                    for i in range(-1, -3, -1):
-                        if (target.tail[0]+i >= 0
-                            and agrid.coord[target.tail[0]+i][target.tail[1]].\
-                                    safe == True):
-                            output.append([target.tail[0]+i, target.tail[1]])
-
-                elif target.tail[0] > target.body[segment][0]:
-                    for i in range(-1, -3, -1):
-                        if (target.tail[0]-i < agrid.height
-                            and agrid.coord[target.tail[0]-i][target.tail[1]].\
-                                    safe == True):
-                            output.append([target.tail[0]-i, target.tail[1]])
-
-        return(output[-1])
+    return(possibilities)
 
 
 def check(direction, y, x, agrid):
@@ -312,21 +305,21 @@ def floodfill(key, moi, agrid):
                  'left': -1, 'right': 1}
 
     sum = 0
-    
+
     if key == 'up' or key == 'down':
         size = check(key, y, x, agrid)
         sum += size
         for dir in direction_parameter['vertical']:
             for i in range(1, size+1):
                 sum += check(dir, y+(i*parameter[key]), x, agrid)
-     
+
     if key == 'left' or key == 'right':
         size = check(key, y, x, agrid)
         sum += size
         for dir in direction_parameter['horizontal']:
             for i in range(1, size+1):
                 sum += check(dir, y, x+(i*parameter[key]), agrid)
-            
+
     return(sum)
 
 
@@ -348,29 +341,15 @@ def move():
     # Initialise board and stuff on board
     data = bottle.request.json
 
-    grid = Grid(data)
-
-    foods = [Food(data['food']['data'][i])
-             for i in range(len(data['food']['data']))]
-
-    me = Me(data['you'], foods)
-
-    enemies = [Enemy(data['snakes']['data'][i], me, foods)
-               for i in range(len(data['snakes']['data']))
-               if data['snakes']['data'][i]['id'] != me.id]
-
-    # Grid for log purposes
-    grid.food_place(foods)
-    for enemoir in enemies: grid.enemy_place(enemoir)
-    grid.me_place(me)
-    grid.print()
+    game = Game(data)
 
     # Route setter
-    safety, backup_safety = safe(grid, me, enemies, data)
+    safe_directions = game.safe_directions()
+
     flooding_safe = floodfill_reorder(safety, me, grid)
     flooding_backup = floodfill_reorder(backup_safety, me, grid)
     goal, output_log = goal_set(me, enemies, grid)
-    route = path(me, goal, grid)
+    route = path(me.head, goal, grid)
     flooding_route = floodfill_reorder(route, me, grid)
 
     output = None
